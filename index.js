45607f790b8c62da67e732c5b8c39863d461f125
alih6051 <alih6051@gmail.com>
2023-03-05 15:13:44 +0530

    stripe added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { connection } = require("./configs/db");
 const { UserRouter } = require("./routes/User.route");
 const { ProductRouter } = require("./routes/Product.route");
 const { AdminRouter } = require("./routes/Admin.route");
+const { StripeRouter } = require("./routes/Stripe.route");
 require("dotenv").config();
 const cors = require("cors");
 
@@ -12,13 +13,14 @@ app.use(cors());
 
 // HOME
 app.get("/", (req, res) => {
-  res.send("ELECTRO GLAD");
+  res.send("DIGI BASKET BACKEND");
 });
 
 // ROUTES
 app.use("/api/users", UserRouter);
 app.use("/api/products", ProductRouter);
 app.use("/api/admin", AdminRouter);
+app.use("/api/stripe-create-checkout", StripeRouter);
 
 app.listen(4500, async () => {
   try {
